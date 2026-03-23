c9d2b050a211e63187a30058a557531af4ef543d
Ibrahim21k <ibrahimkml20@gmail.com>
2026-03-23 09:12:04 +0200

    feat(core): introduce Block type and NewTimestamp helper

--- internal/core/types.go
+++ internal/core/types.go
@@ -1,5 +1,7 @@
 package core
 
+import "time"
+
 type TxType string
 
 const (
@@ -27,4 +29,19 @@ type DomainRecord struct {
     UpdatedAt    int64  `json:"updated_at"`
     LastTxHash   string `json:"last_tx_hash"`
     LastBlockNum uint64 `json:"last_block_num"`
+}
+
+type Block struct {
+    Height       uint64      `json:"height"`
+    PrevHash     string      `json:"prev_hash"`
+    Timestamp    int64       `json:"timestamp"`
+    ValidatorPub string      `json:"validator_pub"`
+    Tx           Transaction `json:"tx"`
+    TxHash       string      `json:"tx_hash"`
+    BlockHash    string      `json:"block_hash"`
+    BlockSig     string      `json:"block_sig"`
+}
+
+func NewTimestamp() int64 {
+    return time.Now().Unix()
 }
