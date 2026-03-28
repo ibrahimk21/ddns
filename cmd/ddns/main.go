50bfde603822fae20b9700cb384fe5e8ad8569e0
Ibrahim21k <ibrahimkml20@gmail.com>
2026-03-28 17:51:33 +0300

    feat(cli): register and update commands with signed tx submission

--- cmd/ddns/main.go
+++ cmd/ddns/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+    "bytes"
     "encoding/json"
     "errors"
     "flag"
     "fmt"
+    "io"
     "net/http"
     "os"
     "strings"
@@ -32,6 +34,10 @@ func main() {
         err = runKeygen()
     case "serve":
         err = runServe(os.Args[2:])
+    case "register":
+        err = runSubmit(os.Args[2:], core.TxRegister)
+    case "update":
+        err = runSubmit(os.Args[2:], core.TxUpdate)
     default:
         printUsage()
         os.Exit(1)
@@ -48,6 +54,8 @@ func printUsage() {
     fmt.Println("commands:")
     fmt.Println("  keygen")
     fmt.Println("  serve --addr :8080 --db chain.db --validator-key validator.json --validators <pub1,pub2,pub3>")
+    fmt.Println("  register --rpc http://localhost:8080/rpc --key owner.json --domain example.com --ip 1.2.3.4 --ttl 3600 --version 1")
+    fmt.Println("  update --rpc http://localhost:8080/rpc --key owner.json --domain example.com --ip 5.6.7.8 --ttl 3600 --version 2")
 }
 
 func runKeygen() error {
@@ -111,4 +119,77 @@ func runServe(args []string) error {
     srv := node.NewServer(chain)
     fmt.Println("listening on", *addr)
     return http.ListenAndServe(*addr, srv.Routes())
+}
+
+func runSubmit(args []string, txType core.TxType) error {
+    fs := flag.NewFlagSet(string(txType), flag.ContinueOnError)
+    rpcURL := fs.String("rpc", "http://localhost:8080/rpc", "JSON-RPC endpoint")
+    keyPath := fs.String("key", "", "owner key JSON")
+    domain := fs.String("domain", "", "domain name")
+    ip := fs.String("ip", "", "IPv4")
+    ttl := fs.Uint("ttl", 3600, "TTL in seconds")
+    version := fs.Uint64("version", 0, "domain version")
+    if err := fs.Parse(args); err != nil {
+        return err
+    }
+
+    if *keyPath == "" || *domain == "" || *ip == "" || *version == 0 {
+        return errors.New("missing required flags")
+    }
+
+    key, err := loadKey(*keyPath)
+    if err != nil {
+        return err
+    }
+
+    tx := core.Transaction{
+        Type:      txType,
+        Domain:    *domain,
+        IP:        *ip,
+        TTL:       uint32(*ttl),
+        OwnerPub:  key.PublicKey,
+        Version:   *version,
+        Timestamp: core.NewTimestamp(),
+    }
+    msg, err := tx.SigningBytes()
+    if err != nil {
+        return err
+    }
+    sig, err := core.SignHex(key.PrivateKey, msg)
+    if err != nil {
+        return err
+    }
+    tx.Signature = sig
+
+    req := map[string]any{
+        "jsonrpc": "2.0",
+        "method":  "submitTx",
+        "params":  tx,
+        "id":      1,
+    }
+
+    return postAndPrint(*rpcURL, req)
+}
+
+func postAndPrint(url string, payload any) error {
+    raw, err := json.Marshal(payload)
+    if err != nil {
+        return err
+    }
+    resp, err := http.Post(url, "application/json", bytes.NewReader(raw))
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return err
+    }
+    var pretty bytes.Buffer
+    if err := json.Indent(&pretty, body, "", "  "); err == nil {
+        fmt.Println(pretty.String())
+    } else {
+        fmt.Println(string(body))
+    }
+    return nil
 }
